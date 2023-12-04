a517c2f7af1b294a2b4ba57426072d9c59ea603f
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-04 16:17:43 +0200

    Create yard_greening.py

---
+++ (A) First_steps_in_coding/Lab/yard_greening.py
@@ -0,0 +1,4 @@
+quadrature = float(input())
+discount = (quadrature * 7.61) * 0.18
+final_price = quadrature * 7.61 - discount
+print(f'The final price is: {final_price:.2f} lv.\nThe discount is: {discount:.2f} lv.')
