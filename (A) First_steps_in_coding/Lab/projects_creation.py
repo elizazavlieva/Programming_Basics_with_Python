20631f29dcdf9b0f5a4ccc879d691d8ee8b000ad
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-04 16:08:13 +0200

    Create projects_creation.py

---
+++ (A) First_steps_in_coding/Lab/projects_creation.py
@@ -0,0 +1,5 @@
+name, num_of_projects = input(), int(input())
+result = num_of_projects * 3
+print(f'The architect {name} '
+      f'will need {result} hours '
+      f'to complete {num_of_projects} project/s.')
