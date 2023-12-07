e4e426ab7524cb4f97fd1dfa653a5e23b37255f9
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2023-12-07 15:41:02 +0200

    Create Conditional Statements - Lab

---
+++ (B) Conditional_statements/Lab/area_of_figures.py
@@ -0,0 +1,24 @@
+import math
+from math import pi
+figure = input()
+area = 0
+
+if figure == 'square':
+    a = float(input())
+    area = a * a
+
+elif figure == 'rectangle':
+    a = float(input())
+    b = float(input())
+    area = a * b
+
+elif figure == 'circle':
+    r = float(input())
+    area = pi * r * r
+
+elif figure == 'triangle':
+    a = float(input())
+    h = float(input())
+    area = (a * h)/2
+
+print(round(area, 3))
---
+++ (B) Conditional_statements/Lab/password_guess.py
@@ -0,0 +1,9 @@
+customer_input = input()
+
+password = 's3cr3t!P@ssw0rd'
+
+if customer_input == password:
+    print('Welcome')
+else:
+    print('Wrong password!')
+    
